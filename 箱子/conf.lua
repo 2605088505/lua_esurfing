ff5410d2b2e3138f379948816e72a88a62a5caa0
2605088505 <66982514+2605088505@users.noreply.github.com>
2021-03-15 17:21:36 +0800

    Update and rename bin/conf.lua to 箱子/conf.lua

--- 箱子/conf.lua
+++ 箱子/conf.lua
@@ -1,19 +1,19 @@
 #!/usr/bin/lua
 
 -- 内网ip地址 即WAN口IP
-clientip='0.0.0.0'
+clientip='100.2.187.21'
 
 -- 认证服务器地址
-nasip='0.0.0.0'
+nasip='119.146.175.80'
 
 -- mac地址，字母大写，使用-分割
-mac=''
+mac='f4-d1-08-5e-8b-21'
 
 -- 安全码，无需修改
 secret='Eshore!@#'
 
 -- 用户名
-username=''
+username='3119001557'
 
 -- 密码
-password=''
+password='12122972'
